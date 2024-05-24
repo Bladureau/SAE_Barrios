c9c6ef6fe9fa2a40c25cb595c2ccd6a63f875874
Baptiste Ladureau <baptiste.ladureau@iut-rodez.fr>
2024-05-24 14:14:59 +0200

    modified:   bin/iut/sae/td1/reel/CalculCaractere.class 	modified:   src/iut/sae/td1/reel/CalculCaractere.java

--- src/iut/sae/td1/reel/CalculCaractere.java
+++ src/iut/sae/td1/reel/CalculCaractere.java
@@ -3,12 +3,8 @@ package iut.sae.td1.reel;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
